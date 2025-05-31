f35281372d5e6b6f678dea993488827e75ad4d74
ilies1511 <iziane@student.42heilbronn.de>
2025-06-01 00:40:42 +0200

    Add containerized server setup using pure Node.js

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((request, response) => {
+	console.log(request.url, request.method);
+	// console.log(request);
+	response.setHeader('Content-Type', 'text/html');
+
+	let path = './files/';
+
+	switch(request.url)
+	{
+		case '/':
+			path += 'index.html';
+			response.statusCode = 200;
+			break ;
+		case '/about':
+			path += 'about.html';
+			response.statusCode = 200;
+			break;
+		case '/about-us':
+			response.statusCode = 307;
+			response.setHeader('Location', '/about')
+			response.end();
+			break;
+		default:
+			path += '404.html';
+			response.statusCode = 404;
+			break ;
+	}
+
+	// Sending HTML Page
+	fs.readFile(path, (err, data) => {
+		if (err) {
+			console.log(err);
+			response.end();
+		}
+		else {
+			// response.write(data);
+			response.end(data);
+		}
+	});
+
+	// // response.write('Ouey Zizou');
+	// response.write('<head> <link rel="styleseet" href="#"> </head>');
+	// response.write('<p>Ouey Zizou</p>');
+	// response.write('<p>Ouey Zizou Goaaaal</p>');
+	// response.end();
+});
+
+server.listen(3000, '0.0.0.0', () => {
+	console.log('listening for requests on port 3000');
+});
