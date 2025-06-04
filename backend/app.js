8afbdf222f59f5d8070b8140c73e5f4664c6bc86
ilies1511 <iziane@student.42heilbronn.de>
2025-06-04 12:28:20 +0200

    Add own middleware implementation

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,19 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use((req, res, next) => {
+	console.log('new request made');
+	console.log('host: ', req.hostname);
+	console.log('path: ', req.path);
+	console.log('method: ', req.method);
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log('In the next middleware');
+	next();
+});
+
 //listens for request
 app.get('/', (req, res) => {
 	// res.render('index', {
