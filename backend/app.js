1c653fb94910d70440f627195d05d6440cd7ae0c
ilies1511 <iziane@student.42heilbronn.de>
2025-06-09 21:08:06 +0200

    FEAT(routing): Add routing for more modularity

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes');
 
 //here, we are invoking that fnc to create an instance of express app --> gets stored in 'app'
 const app = express();
@@ -123,59 +123,59 @@ app.get('/about', (req, res) => {
 	// res.send('<p>About Zizou</p>');
 });
 
-app.get('/blogs', (req, res) => {
-	Blog.find().sort({createdAt: -1})
-		.then((result) => {
-			res.render('index', {
-				title: 'All Blogs',
-				blogs: result
-			})
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-})
+// app.get('/blogs', (req, res) => {
+// 	Blog.find().sort({createdAt: -1})
+// 		.then((result) => {
+// 			res.render('index', {
+// 				title: 'All Blogs',
+// 				blogs: result
+// 			})
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		})
+// })
 
-app.post('/blogs', (req, res) => {
-	console.log(req.body);
-	const blog = new Blog(req.body);
+// app.post('/blogs', (req, res) => {
+// 	console.log(req.body);
+// 	const blog = new Blog(req.body);
 
-	blog.save()
-		.then((result) => {
-			res.redirect('/blogs');
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-});
+// 	blog.save()
+// 		.then((result) => {
+// 			res.redirect('/blogs');
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		})
+// });
 
-app.get('/blogs/create', (req, res) => {
-	res.render('create', {
-		title: 'Bob der Baumeister'
-	});
-});
+// app.get('/blogs/create', (req, res) => {
+// 	res.render('create', {
+// 		title: 'Bob der Baumeister'
+// 	});
+// });
 
-app.get('/blogs/:id', (req, res) => {
-	const id = req.params.id;
-	Blog.findById(id)
-		.then(result => {
-			res.render('details', { blog: result, title: 'Blog Details '})
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+// app.get('/blogs/:id', (req, res) => {
+// 	const id = req.params.id;
+// 	Blog.findById(id)
+// 		.then(result => {
+// 			res.render('details', { blog: result, title: 'Blog Details '})
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 		});
+// });
 
-app.delete('/blogs/:id', (req, res) => {
-	const id = req.params.id;
-	Blog.findByIdAndDelete(id)
-		.then(result => {
-			res.json({ redirect: '/blogs '});
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+// app.delete('/blogs/:id', (req, res) => {
+// 	const id = req.params.id;
+// 	Blog.findByIdAndDelete(id)
+// 		.then(result => {
+// 			res.json({ redirect: '/blogs '});
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 		});
+// });
 
 // app.get('/blogs/create', (req, res) => {
 // 	res.render('create', {
@@ -187,6 +187,11 @@ app.delete('/blogs/:id', (req, res) => {
 app.get('/about-us', (req, res) => {
 	res.redirect('/about');
 });
+
+/* Blog Routes */
+
+// app.use(blogRoutes);
+app.use('/blogs', blogRoutes);
 
 /*
 	Gefaehrlich, da not scopped out to specific URL, hence will be triggered
