499c1432f27a5223d71a686abe8d6334d2d412cf
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2025-06-23 23:28:14 +0200

    not work

--- core/frontend/vite.config.ts
+++ core/frontend/vite.config.ts
@@ -1,45 +1,52 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [],
-  server: {
-	host: "0.0.0.0",
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-		rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
-// import path from 'path' // <-- Required for path.resolve
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite';
 
 // export default defineConfig({
-//   plugins: [tailwindcss()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'client'), // optional but useful for client code
-//     }
-//   },
+//   plugins: [],
 //   server: {
 // 	host: "0.0.0.0",
+//     port: 3001,
 //     proxy: {
-//       '/api': 'http://localhost:3000',
-//       '/ws': {
-//         target: 'ws://localhost:3000',
-//         ws: true
-//       },
-//       '/game': {
-//         target: 'ws://localhost:3000',
-//         ws: true
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//         secure: false,
+// 		rewrite: (path) => path.replace(/^\/api/, '')
 //       }
 //     }
 //   }
-// })
+// });
+
+
+import path from 'path' // <-- Required for path.resolve
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client'), // optional but useful for client code
+    }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      '/api': {
+			target: 'http://localhost:3000',
+	  		changeOrigin: true,
+           secure: false
+	  },
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true
+      },
+      '/game': {
+        target: 'ws://localhost:3000',
+        ws: true
+      }
+    }
+  }
+})
 
 
