a336329f3487443a61704ca22d84b17fc82278fe
FabianRapp <fa.rapp@web.de>
2025-06-24 16:22:42 +0200

    new file structure with git history

--- game_shared/math.ts
+++
@@ -1,9 +0,0 @@
-
-import { SharedVec2 } from './objects/SharedVec2.ts'
-import { SharedBall } from './objects/SharedBall.ts'
-import { SharedWall } from './objects/SharedWall.ts'
-import { SharedClient } from './objects/SharedClient.ts'
-
-
-export const EPSILON: number = 1e-6;
-
