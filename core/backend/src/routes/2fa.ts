08143fa37b4628cf7f00937f57264cf984c8de64
ilies1511 <iziane@student.42heilbronn.de>
2025-07-22 02:18:22 +0200

    CHORE(2FA): Add test workflow

--- core/backend/src/routes/2fa.ts
+++ core/backend/src/routes/2fa.ts
@@ -6,43 +6,46 @@ export const twoFaRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) 
 	fastify.post(
 		'/api/2fa/generate',
 		{ preHandler: [fastify.auth] }, //Needed ?
+		// // {
+		// // 	schema: {
+		// // 		tags: ['auth'],
+		// // 		body: {
+		// // 			type: 'object',
+		// // 			required: ['token'],
+		// // 			properties: { token: { type: 'string' } }
+		// // 		},
+		// // 		response: {
+		// // 			200: { type: 'object', properties: { success: { type: 'boolean' } } },
+		// // 			400: { type: 'object', properties: { error: { type: 'string' } } }
+		// // 		}
+		// // 	},
+		// // // BEGIN -- Working Scheme
 		// {
+		// 	preHandler: [fastify.auth],
 		// 	schema: {
 		// 		tags: ['auth'],
-		// 		body: {
-		// 			type: 'object',
-		// 			required: ['token'],
-		// 			properties: { token: { type: 'string' } }
-		// 		},
+		// 		summary: 'Initializes 2FA',
+		// 		description: 'Generiert ein QR-Code-Bild als Data-URL. Authentifizierung erfolgt über das Cookie `token`.',
 		// 		response: {
-		// 			200: { type: 'object', properties: { success: { type: 'boolean' } } },
-		// 			400: { type: 'object', properties: { error: { type: 'string' } } }
+		// 			200: {
+		// 				description: 'QR-Code succesfully generated',
+		// 				type: 'object',
+		// 				properties: {
+		// 					qr: { type: 'string', description: 'Data-URL of a QR-Code' }
+		// 				}
+		// 			},
+		// 			400: {
+		// 				description: 'Invalid Token or other error',
+		// 				type: 'object',
+		// 				properties: {
+		// 					error: { type: 'string' }
+		// 				}
+		// 			}
 		// 		}
-		// 	},
-			// schema: {
-			// 	tags: ['auth'],
-			// 	summary: 'Initialisiert die Zwei-Faktor-Authentifizierung',
-			// 	description: 'Generiert ein QR-Code-Bild als Data-URL. Authentifizierung erfolgt über das Cookie `token`.',
-			// 	response: {
-			// 		200: {
-			// 			description: 'QR-Code erfolgreich generiert',
-			// 			type: 'object',
-			// 			properties: {
-			// 				qr: { type: 'string', description: 'Data-URL eines QR-Codes' }
-			// 			}
-			// 		},
-			// 		400: {
-			// 			description: 'Ungültiger Token oder anderer Fehler',
-			// 			type: 'object',
-			// 			properties: {
-			// 				error: { type: 'string' }
-			// 			}
-			// 		}
-			// 	}
-			// },
-			// preHandler: [fastify.auth]
+		// 	}
+		// 	// preHandler: [fastify.auth]
 		// },
-		// { preHandler: [fastify.auth] }, //Needed ?
+		// // // END -- Working Scheme
 		async (req, reply) => {
 			const userId = (req.user as any).id
 			console.log("USER ID 2FA:" + userId);
@@ -97,3 +100,38 @@ export const twoFaRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) 
 	)
 }
 
+/*
+	TEST FLOW:
+
+	1. Normal Login via /api/login to get JWT
+		curl -i -X POST http://localhost:3000/api/login \
+		-H 'Content-Type: application/json' \
+		-d '{"email":"alice@example.com","password":"geheim"}'
+
+	2. Generate SECRET & QR-Code (simpyfies setup)
+		curl -i -X POST http://localhost:3000/api/2fa/generate \
+		-H "Cookie: token=<USER_JWT_TOKEN>"
+// --cookie "token=<USER_JWT_TOKEN>"
+
+	3. Verifying TOTP Code -> provided by GoogleAuthentifactor after 2 step
+		curl -i -X POST http://localhost:3000/api/2fa/verify \
+		--cookie "token=<USER_JWT_TOKEN>" \
+		-H 'Content-Type: application/json' \
+		-d '{"token":"123456"}'
+
+	4. Now 2FA is set-up for user
+
+	5. Starting from now the user is logging in with the 2fa login --> /api/login/2fa
+		curl -i -X POST http://localhost:3000/api/login/2fa \
+		-H 'Content-Type: application/json' \
+		-d '{"email":"alice@example.com","password":"geheim","token":"123456"}'
+
+	TL;DR:
+	1. Login -> 'extracting' Cookie token
+	2. Generate -> scan QR-Code (via external App/Tool)
+	3. Verify -> paste in TOTP-Codes
+	4. 2FA-Login:
+		- if user tries now via '/api/login', he will be 'redirected' to
+			'/api/login/2fa' and asked to enter TOTP-Codes
+		- now login via '/api/login/2fa'
+ */
