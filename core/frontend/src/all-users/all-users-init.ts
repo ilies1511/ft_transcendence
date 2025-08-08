bfbc65ef5cfd703fba7e2c0728fb9739b8818518
FabianRapp <fa.rapp@web.de>
2025-08-08 22:33:43 +0200

    feat(added some lobby invite logic)

--- core/frontend/src/all-users/all-users-init.ts
+++ core/frontend/src/all-users/all-users-init.ts
@@ -3,6 +3,15 @@ import { wsEvents } from '../services/websocket';
 import { template, wireEvents, unWireEvents } from './all-users-ui';
 import { fetchAndFill } from './all-users-list';
 import { inviteToast } from '../ui/inviteToast';
+import type { LobbyInvite, ServerError } from '../game/game_shared/message_types.ts';
+import {
+	accept_lobby_invite,
+	create_join_lobby,
+	invite_user_to_lobby_skeleton,
+	recv_lobby_invite_skeleton,
+} from '../game/frontend_interface_examples/custom_lobbies.ts';
+import { Game } from '../game/game_new.ts'
+
 
 export async function initAllUsersUI() {
 	const root = document.getElementById('all-users-ui-root');
@@ -22,12 +31,27 @@ export async function initAllUsersUI() {
 	wsEvents.addEventListener('lobby_invite', handleLobbyInvite);
 }
 
-function handleLobbyInvite(ev: Event) {
+async function handleLobbyInvite(ev: Event) {
 	const { from, content } = (ev as CustomEvent).detail;
+	const invite: LobbyInvite = content as LobbyInvite;
 
 	inviteToast(`User #${from} invited you to play`);
 
 	console.log('[LobbyInvite] from user', from, content);
+
+	//const game: Game | ServerError = await accept_lobby_invite(
+	//	user_id,
+	//	container,
+	//	invite,
+	//	display_name,
+	//);
+
+	//if (game == '') {
+	//	//game should be running
+	//	return ;
+	//} else {
+	//	console.log("Error with lobby invite: ", game);
+	//}
 }
 
 export function destroyAllUsersUI() {
