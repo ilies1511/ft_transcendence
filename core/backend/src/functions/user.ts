68cb4663134a7a352acba2340cd898081eed48d6
ilies1511 <iziane@student.42heilbronn.de>
2025-08-25 23:13:01 +0200

    FEAT(live): Add helper fnc to get live player count

--- core/backend/src/functions/user.ts
+++ core/backend/src/functions/user.ts
@@ -192,3 +192,11 @@ export async function updateUserAvatar(
 export async function getUserId(request: any) {
 	return (request.user as any).id as number;
 }
+
+//Displays Live Player Count --> for Fabi
+export async function getLivePlayersCount(fastify: FastifyInstance): Promise<number> {
+	const row = await fastify.db.get<{ c: number }>(
+		'SELECT COUNT(*) AS c FROM users WHERE live = 1 AND is_deleted = 0'
+	);
+	return row?.c ?? 0;
+}
